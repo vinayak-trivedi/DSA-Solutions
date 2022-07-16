6be2e88b8f521774edbee58fa93faaf117c64149
vinayak-trivedi <90315175+vinayak-trivedi@users.noreply.github.com>
2022-07-16 10:03:43 +0530

    added second solution

---
+++ first-java/solutions/src/com/vinayak/Solution2.java
@@ -0,0 +1,13 @@
+package com.vinayak;
+
+import java.util.Scanner;
+
+public class Solution2 {
+    public static void main(String[] args) {
+        System.out.print("Enter your name: ");
+        Scanner input = new Scanner(System.in);
+        String name = input.nextLine();
+        System.out.println("Hello " + name);
+    }
+
+}
