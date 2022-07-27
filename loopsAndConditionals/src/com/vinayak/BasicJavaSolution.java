d25d2e6957616eaa1cc95b3055612d1cb798a9e0
vinayak-trivedi <90315175+vinayak-trivedi@users.noreply.github.com>
2022-07-27 21:17:55 +0530

    added some solution for the basic question

---
+++ loopsAndConditionals/src/com/vinayak/BasicJavaSolution.java
@@ -0,0 +1,68 @@
+package com.vinayak;
+
+import java.util.Scanner;
+
+public class BasicJavaSolution {
+
+    public static void main(String[] args) {
+
+        // commenting all the question. They are very basic so not making different classes for each solution.
+
+        // find the area of circle
+        Scanner input = new Scanner(System.in);
+//        System.out.print("Enter radius to know the area: ");
+//        int r = input.nextInt();
+//        double area = 3.14 * r * r;
+//        System.out.println("Area of circle: " + area);
+
+        // find the area of triangle
+//        System.out.print("Enter base and height: ");
+//        int b = input.nextInt();
+//        int h = input.nextInt();
+//        double area = 0.5 * (b * h);
+//        System.out.println("Area of triangle: " + area);
+
+        // find the area of rectangle
+//        System.out.print("Enter width and height: ");
+//        int w = input.nextInt();
+//        int l = input.nextInt();
+//        int area = w * l;
+//        System.out.println("Area of rectangle: " + area);
+
+        // find the area of Isosceles triangle
+
+//        System.out.print("Enter base and height: ");
+//        int b = input.nextInt();
+//        int h = input.nextInt();
+//        double area = 0.5 * (b * h);
+//        System.out.println("Area of triangle: " + area);
+
+        // Find the area of parallelogram
+//        System.out.print("Enter Base and Height: ");
+//        int b = input.nextInt();
+//        int h = input.nextInt();
+//        int area = b * h;
+//        System.out.println("Area: " + area);
+//
+        // Find the area of rhombus using diagonals
+//        System.out.print("Enter two diagonals: ");
+//        int d1 = input.nextInt();
+//        int d2 = input.nextInt();
+//        double area = 0.5 * (d1 * d2);
+//        System.out.println("Area: " + area);
+
+        // Find the area of equilateral triangle
+//        System.out.print("Enter the value of side: ");
+//        int a = input.nextInt();
+//        double area = 1.732/4 * (a * a);
+//        System.out.println(area);
+
+        // Perimeter of circle
+//        System.out.println("Enter the radius");
+//        double r = input.nextInt();
+//        double area = 2 * 3.14 * r;
+//        System.out.println("Area: " +
+
+        // Leaving some question as they are repetitive.
+    }
+}
