6ee8abd5de483f5eade0869eef5e0cf1c1e8b4eb
vinayak-trivedi <90315175+vinayak-trivedi@users.noreply.github.com>
2022-08-03 20:46:35 +0530

    Solution question one

---
+++ ArrayAndArrayList/src/com/vinayak/PermuationArray.java
@@ -0,0 +1,22 @@
+package com.vinayak;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class PermuationArray {
+    public static void main(String[] args) {
+        // Question link: https://leetcode.com/problems/build-array-from-permutation/
+        int[] arr = {0,2,1,5,3,4};
+        int [] ans = answerArry(arr);
+        System.out.println(Arrays.toString(ans));
+    }
+
+    static int[] answerArry(int[] arr) {
+        int[] ans = new int[arr.length];
+        for (int i = 0; i < arr.length; i++) {
+            ans[i] = arr[arr[i]];
+        }
+        return ans;
+    }
+}
+
