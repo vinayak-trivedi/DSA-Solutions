d999f10bf2c16601c0dda2fc3a3685b9afdfa887
vinayak <trivedivinayak57@gmail.com>
2022-09-22 13:34:59 +0530

    solution for searching in a roatated array

--- Binary-search/src/com/vinayak/RotatedArrayBinarySearch.java
+++ Binary-search/src/com/vinayak/RotatedArrayBinarySearch.java
@@ -12,43 +12,59 @@ public class RotatedArrayBinarySearch {
 
         // so how do we find an element in a rotated array
 
-        // if we observe the array we can see that the highest number is going to be the one which is from which
-        // the array is rotated
-        // and after that there is a drop
-        // so we will find the peak in the array and search till peak
-        // if we find the target element then we are done
-        // if not then we find after the peak element
+        // first we will find the pivot in the array, which is the smallest number basically, let us understand with
+        // an example how it helps
+        // suppose we have a rotated array {5, 6, 1, 2, 3, 4}
+        // so we find the pivot which is the smallest number which is going to be 1 in our case
+        // we can observe here that we have two sorted array now, one which is from the 0th index till the pivot
+        // and second which is from pivot + 1 index till arr.length -1;
+        // so we will run binary search there
 
-        int[] arr = {5, 6, 1, 2, 3, 4}; // rotated two times
-        int target = 2;
+        // how will we find the pivot
+        // supposing that we have two array/
+        // all the elements in the first pivot are going to be bigger than all the elements in the second one
+        // so our pivot will lie somewhere in the second pivot
+        // so we check if the arr[0] <= arr[mid] in this case we are in the first sorted array, we need to move to
+        // the next one, so we need to go ahead , so we do start = mid + 1;
+        // else case is going to be the one where we are in the second array, then we will move the end below because
+        // we want to get the smallest number there
 
-        int peak = findPeak(arr);
-        int firstTry = binarySearch(arr, target, 0, peak);
+        // this loop will run until start < end because when start = end which means that there is only one number left
+        // in the array, which is going to be the smallest number because of the above conditions
 
-        if( firstTry != -1) {
+        // code
+
+        int[] arr = {4,5,6,7,0,1,2};
+
+        int target = 1;
+
+        int pivot = findPivot(arr);
+        System.out.println(pivot);
+        int firstTry = binarySearch(arr, target, 0, pivot - 1);
+        if(firstTry != -1) {
             System.out.println(firstTry);
-            System.out.println("Found the answer in first try!");
+            System.out.println("found in first try");
         } else {
-            int ans = binarySearch(arr, target, peak + 1, arr.length -1);
-            System.out.println(ans);
+            System.out.println(binarySearch(arr, target, pivot, arr.length -1));
         }
+
     }
 
-    static int findPeak(int[] arr) {
+    static int findPivot(int[] arr) {
         int start = 0;
         int end = arr.length - 1;
 
-        while (start <= end) {
-            int mid = start + (end - start) /2;
-            if (arr[mid] > arr[mid + 1]) {
-                return mid;
-            } else if (arr[start] < arr[mid]) {
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[0] <= arr[mid]) { // we are in the part of the array which is before pivot
                 start = mid + 1;
             } else {
-                end = mid; // we are doing mid here because we do not know if the previous element there is an answer
+                end = mid; // why we are taking mid because we don't know if the number is pivot so we will get to
+                // place which is before the pivot which is not correct
             }
         }
-        return -1;
+        return start; // we can return anything start or end because they are same
     }
 
     static int binarySearch(int arr[], int target, int start, int end) {
