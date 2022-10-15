e637e858e3f7b5e674084787e7675c1ca0a26192
vinayak <trivedivinayak57@gmail.com>
2022-10-15 15:01:38 +0530

    added enhanced solution for bubble sort

--- Sorting/JavaScript/bubbleSortBasic.js
+++ Sorting/JavaScript/bubbleSortBasic.js
@@ -3,14 +3,21 @@
 const arr = [11, 23, 8, 7, 10, 16, 14, 15];
 
 console.log("unsorted Array: ", arr)
+let swapped;
 
 for (let i = 0; i < arr.length - 1; i++) {
+  swapped = false;
   for (let j = 0; j < arr.length - 1 - i; j++) {
     if (arr[j] > arr[j + 1]) {
       const temp = arr[j];
       arr[j] = arr[j + 1];
       arr[j + 1] = temp;
+      swapped = true;
     }
+  }
+  // if no value is swapped in the above loop it means that our array is sorted and there is no reason that we should go ahead with other iterations in the loop
+  if (!swapped) {
+    break;
   }
 }
 
