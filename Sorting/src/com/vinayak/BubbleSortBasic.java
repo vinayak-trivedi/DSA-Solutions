9db74d56a8d530622373abec981315d1c259807c
vinayak <trivedivinayak57@gmail.com>
2022-10-15 15:11:26 +0530

    enhanced the Java solution for bubble sort

--- Sorting/src/com/vinayak/BubbleSortBasic.java
+++ Sorting/src/com/vinayak/BubbleSortBasic.java
@@ -5,15 +5,21 @@ import java.util.Arrays;
 public class BubbleSortBasic {
     public static void main(String[] args) {
         // sort the given array using bubble sort
-        int[] arr = {11, 8, 2, 22, 16, 7};
-
+        int[] arr = {1, 2, 3, 4, 5};
+        boolean swapped;
         for (int i = 0; i < arr.length - 1; i++) {
+            swapped = false;
             for (int j = 0; j < arr.length - 1 - i ; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
+                    swapped = true;
                 }
+            }
+
+            if (!swapped) {
+                break;
             }
         }
         System.out.println("Sorted array: " + Arrays.toString(arr));
