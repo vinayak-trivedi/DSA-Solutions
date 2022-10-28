ed439efedb48b4e165c7be2b3f068c40ab97d38c
vinayak <trivedivinayak57@gmail.com>
2022-10-28 16:52:19 +0530

    JS solution for binary search with recursion

---
+++ Recursion/JavaScript/binarySearchRecursion.js
@@ -0,0 +1,19 @@
+// Do binary search on a sorted array using recursion
+function binarySearchRecursion(arr, target, start, end) {
+  let mid = start + (end - start)/2;
+
+  if (arr[mid] === target) {
+    return mid;
+  }
+
+  if (arr[mid] > target) {
+    return binarySearchRecursion(arr, target, start, mid - 1);
+  }
+
+  return binarySearchRecursion(arr, target, mid + 1, end);
+}
+
+const array = [1, 2, 3, 5, 7, 66, 78];
+const target = 66;
+
+console.log("index of target is", binarySearchRecursion(array, target, 0, array.length - 1));
